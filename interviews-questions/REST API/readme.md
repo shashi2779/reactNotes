7fa7ec3076f255827f02c17dfaeb20c6f99dc0e8
shashi2779 <yadavshashikant2779@gmail.com>
2023-04-10 17:22:29 +0530

    ythjty

--- interviews-questions/REST API/readme.md
+++ interviews-questions/REST API/readme.md
@@ -185,3 +185,66 @@ headers: {
 };
 
 ```
+
+```js
+import React, { useState, useEffect } from 'react';
+
+function PostData() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const addPosts = async (title, body) => {
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',  // content type json format me rhega
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setPosts((posts) => [data, ...posts]);
+        setTitle('');
+        setBody('');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // default behaviour change karta hai
+    addPosts(title, body); // upar wale fun ko call kiya
+  };
+
+
+  return (
+    <div className="app">
+      <div className="add-post-container">
+        <form onSubmit={handleSubmit}>
+          {/*  user-Title  */}
+          <input type="text" className="form-control" value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          {/* user-body */}
+          <textarea name="" className="form-control" id="" cols="10" rows="8"
+            value={body} onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <button type="submit" >Add Post</button>
+        </form>
+      </div>
+      {/*  */}
+    </div>
+  )
+}
+
+export default PostData
+
+```
--- interviews-questions/REST API/rest-api-project/src/component/PostData.js
+++ interviews-questions/REST API/rest-api-project/src/component/PostData.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-function PostData() {  
+function PostData() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [posts, setPosts] = useState([]);
 
   const addPosts = async (title, body) => {
-    await fetch('https://jsonplaceholder.typicode.com/posts', { 
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
       body: JSON.stringify({
         title: title,
@@ -14,11 +14,12 @@ function PostData() {
         userId: Math.random().toString(36).slice(2),
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        'Content-type': 'application/json; charset=UTF-8',  // content type json format me rhega
       },
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         setPosts((posts) => [data, ...posts]);
         setTitle('');
         setBody('');
@@ -31,7 +32,7 @@ function PostData() {
 
   const handleSubmit = (e) => {
     e.preventDefault(); // default behaviour change karta hai
-    addPosts(title, body);
+    addPosts(title, body); // upar wale fun ko call kiya
   };
 
 
@@ -39,16 +40,18 @@ function PostData() {
     <div className="app">
       <div className="add-post-container">
         <form onSubmit={handleSubmit}>
+          {/*  user-Title  */}
           <input type="text" className="form-control" value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
+          {/* user-body */}
           <textarea name="" className="form-control" id="" cols="10" rows="8"
             value={body} onChange={(e) => setBody(e.target.value)}
           ></textarea>
-          <button type="submit">Add Post</button>
+          <button type="submit" >Add Post</button>
         </form>
       </div>
-      {/* ... */}
+      {/*  */}
     </div>
   )
 }
